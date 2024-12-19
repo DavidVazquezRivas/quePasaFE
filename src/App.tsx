a3e26b7ce268a188a1dc90a6259a3741dfabf1d0
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-19 10:50:46 +0100

    Wrap App component with UserProvider for user state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { RegisterPage } from '@/pages/Register/RegisterPage'
+import { UserProvider } from './contexts/UserContext'
 
 function App() {
   return (
     <div id="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<h1>Dashboard</h1>} />
-          <Route path="register" element={<RegisterPage />} />
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<h1>Dashboard</h1>} />
+            <Route path="register" element={<RegisterPage />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   )
 }
