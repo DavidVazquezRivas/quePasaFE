a6d4045dda70908c293c9640207e8afcfca9427b
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-19 06:44:39 +0100

    Add routing for Dashboard and RegisterPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { RegisterPage } from '@/pages/Register/RegisterPage'
 
 function App() {
-  return ( 
-    <></>
+  return (
+    <div id="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<h1>Dashboard</h1>} />
+          <Route path="register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
