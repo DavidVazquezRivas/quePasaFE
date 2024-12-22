bc151178e4c1d2e01d54a38b68601dd7fc742ff3
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-22 02:46:26 +0100

    Add ChatList, ChatCard, and ChatListHeader components with polling for user chats

---
+++ src/components/ChatList/ChatList.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from 'react'
+import { Box } from '@mui/material'
+import { ChatCard } from './ChatCard'
+import { getUserChats } from '@/services/chatService'
+import { UserChat } from '@/types/models'
+import { Spinner } from '@/components/Spinner'
+import { ChatListHeader } from './ChatListHeader'
+import { POLLING_INTERVAL } from '@/config/config'
+
+interface ChatListProps {
+  selected?: number
+}
+
+export const ChatList: React.FC<ChatListProps> = ({ selected = 0 }) => {
+  const [chats, setChats] = React.useState<UserChat[] | null>(null)
+
+  // Obtener los chats del usuario cada x tiempo
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const response = await getUserChats()
+        if (response.errorMessage) {
+          console.error('Error controlado:', response.errorMessage)
+        } else {
+          setChats(response.data)
+        }
+      } catch (error) {
+        console.error('Error no controlado:', error)
+      }
+    }
+
+    // Llamar la función al montar el componente
+    fetchChats()
+
+    // Configurar el intervalo para ejecutarla cada 5 segundos
+    const intervalId = setInterval(fetchChats, POLLING_INTERVAL)
+
+    // Limpiar el intervalo al desmontar el componente
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const chatCards = chats ? (
+    chats.map((chat) => {
+      const logo = `https://picsum.photos/${10 + chat.id}`
+      return (
+        <ChatCard
+          key={chat.id}
+          chatName={chat.name}
+          chatLogo={logo}
+          unreadMessages={chat.unreadMessages}
+          selected={chat.id === selected}
+        />
+      )
+    })
+  ) : (
+    <Spinner />
+  )
+
+  return (
+    <Box sx={chatListContainerStyles}>
+      <ChatListHeader />
+
+      <Box component={'ul'} sx={chatListStyles} className="scrollbar-custom">
+        {chatCards}
+      </Box>
+    </Box>
+  )
+}
+
+const chatListContainerStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: 400,
+  maxHeight: '100%',
+  backgroundColor: 'var(--color-light-gray)',
+  boxShadow: 2,
+}
+
+const chatListStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  maxHeight: '100%',
+  padding: 2,
+  gap: 2,
+  backgroundColor: 'var(--color-light-gray)',
+  boxShadow: 2,
+  overflowY: 'auto',
+}
