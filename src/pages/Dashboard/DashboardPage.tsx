f090e42bd11891d257f211d45a404264df248cf1
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-22 13:43:35 +0100

    Add loading state and spinner to DashboardPage during session verification

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -2,21 +2,34 @@ import { ChatList } from '@/components/ChatList/ChatList'
 import { Header } from '@/components/Header'
 import { useSession } from '@/hooks/useSession'
 import { Box } from '@mui/material'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { WelcomeMessage } from './components/WelcomeMessage'
+import { Spinner } from '@/components/Spinner'
 
 export const DashboardPage: React.FC = () => {
   const { checkSession } = useSession()
   const navigate = useNavigate()
+
+  // Estado para verificar la sesión
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    checkSession().then((sessionActive) => {
+    const verifySession = async () => {
+      const sessionActive = await checkSession()
+
       if (!sessionActive) {
         // Si no hay sesión activa, redirigir al usuario a la página de login
         navigate('/login')
+      } else {
+        // Si hay sesión activa, actualizamos el estado de loading
+        setLoading(false)
       }
-    })
-  })
+    }
+
+    verifySession()
+  }, [checkSession, navigate])
+
   const headerSize = 64
 
   return (
@@ -28,13 +41,21 @@ export const DashboardPage: React.FC = () => {
           height: `calc(100vh - ${headerSize}px)`,
           marginTop: `${headerSize}px`,
           width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <ChatList />
-        <WelcomeMessage
-          title="Select a chat"
-          description="Choose a conversation from the list on the left to start chatting."
-        />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <ChatList />
+            <WelcomeMessage
+              title="Select a chat"
+              description="Choose a conversation from the list on the left to start chatting."
+            />
+          </>
+        )}
       </Box>
     </>
   )
